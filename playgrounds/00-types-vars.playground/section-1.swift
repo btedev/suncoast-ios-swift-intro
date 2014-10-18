73fa385107c306da62cb606f3cb5b6a46fa4fecb
Barry Ezell <barrye@gmail.com>
2014-10-17 21:25:24 -0400

    Add 00 playground

---
+++ playgrounds/00-types-vars.playground/section-1.swift
@@ -0,0 +1,17 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+// Types and variables
+
+var pi = 3.14159
+
+var closeEnough: Int = 3.14159
+
+var thisManyFingers: Float = 10
+
+var greeting = "Hello World"
+
+var booleansAreEasy = true
+
+let a = 4
