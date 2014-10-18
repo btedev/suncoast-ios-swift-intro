719ab9e479f58c388ae05e546857e5ceb2bda319
Barry Ezell <barrye@gmail.com>
2014-10-17 23:10:13 -0400

    Update playground 03

--- playgrounds/03-intermediate.playground/section-1.swift
+++ playgrounds/03-intermediate.playground/section-1.swift
@@ -2,7 +2,7 @@
 
 import UIKit
 
-// tuple: species, age
+// animal tuple: species, age
 var cat1 = ("feline", 9)
 var cat2 = ("feline", 1)
 var dog1 = ("canine", 8)
@@ -11,38 +11,28 @@ var dog2 = ("canine", 1)
 dog1.0
 dog1.1
 
-func someDog() -> (kind: String,  age: Int) {
+typealias Animal = (species: String, age: Int)
+
+//func secondDog() -> (species: String, age: Int) {
+func secondDog() -> (Animal) {
     return dog2
 }
 
-var aDog = someDog()
-aDog.kind
+var aDog = secondDog()
+aDog.species
 aDog.age
 
-var animal = someDog()
-animal.kind
-animal.age
-
-typealias Animal = (kind: String, age: Int)
-
+//func description(species: String, age: Int) -> String {
 func description(animal: Animal) -> String {
-    switch animal {
-    case let (kind, age) where age < 2:
-        return "A \(kind) age \(age) is a juvenile"
-        
+    switch(animal) {
+    case let(species, age) where age <= 2:
+        return "A \(species) age \(age) is a juvenile"
     case ("feline", let age) where age >= 12:
         return "A feline age \(age) is a senior"
-        
     case ("canine", let age) where age >= 8:
         return "A canine age \(age) is a senior"
-        
-        /*
-        case let (kind, age):
-        return "A \(kind) age \(age) is an adult"
-        
-        default:
-        return "this will never execute"
-        */
+    case let(species, age):
+        return "A \(species) age \(age) is an adult"
     }
 }
 
@@ -63,4 +53,5 @@ struct Pet {
 }
 
 var p1 = Pet(animal: ("canine", 4), vaccination: Vaccination.Current)
-var p2 = Pet(animal: ("feline", 5), vaccination:Vaccination.Expired(months: 11))
+var p2 = Pet(animal: ("feline", 4), vaccination: Vaccination.Expired(months: 5))
+var p3 = Pet(animal: ("canine", 3), vaccination: .Never)
